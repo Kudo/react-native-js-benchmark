941a0fea295bdb1923767f60c972c1f9fdab153d
Kudo Chien <ckchien@gmail.com>
2022-02-18 14:12:25 +0800

    upgrade for react-native 0.66

--- js_dists.py
+++ js_dists.py
@@ -1,24 +1,9 @@
 JS_DISTS = {
-    'jsc_official_245459': {
-        'download_url':
-        'https://registry.npmjs.org/jsc-android/-/jsc-android-245459.0.0.tgz',
-        'version':
-        '245459.0.0',
-        'meta': ('Baseline JIT (but not x86)', 'WebKitGTK 2.24.2'),
-        'aar_glob':
-        '**/android-jsc/**/*.aar',
-        'binary_name':
-        'libjsc.so',
-        'maven_dist_path':
-        'package/dist',
-        'intl':
-        False,
-    },
     'jsc_250230': {
         'download_url':
-        'https://registry.npmjs.org/@kudo-ci/jsc-android/-/jsc-android-250230.2.1.tgz',
+        'https://registry.npmjs.org/jsc-android/-/jsc-android-250230.2.1.tgz',
         'version':
-        '250230.2.0',
+        '250230.2.1',
         'meta': ('Baseline JIT (but not x86)', 'WebKitGTK 2.26.1'),
         'aar_glob':
         '**/android-jsc/**/*.aar',
@@ -29,12 +14,12 @@ JS_DISTS = {
         'intl':
         False,
     },
-    'v8_80_nointl': {
+    'v8_93_nointl': {
         'download_url':
-        'https://registry.npmjs.org/v8-android-nointl/-/v8-android-nointl-8.80.1.tgz',
+        'https://registry.npmjs.org/v8-android-nointl/-/v8-android-nointl-9.93.0.tgz',
         'version':
-        '8.80.1',
-        'meta': ('JIT-less', 'V8 8.0.426.16'),
+        '9.93.0',
+        'meta': ('JIT-less', 'V8 9.3.345.16'),
         'aar_glob':
         '**/*.aar',
         'binary_name':
@@ -44,12 +29,12 @@ JS_DISTS = {
         'intl':
         False,
     },
-    'v8_80_jit_nointl': {
+    'v8_93_jit_nointl': {
         'download_url':
-        'https://registry.npmjs.org/v8-android-jit-nointl/-/v8-android-jit-nointl-8.80.1.tgz',
+        'https://registry.npmjs.org/v8-android-jit-nointl/-/v8-android-jit-nointl-9.93.0.tgz',
         'version':
-        '8.80.1',
-        'meta': ('JIT', 'V8 8.0.426.16'),
+        '9.93.0',
+        'meta': ('JIT', 'V8 9.3.345.16'),
         'aar_glob':
         '**/*.aar',
         'binary_name':
@@ -59,11 +44,11 @@ JS_DISTS = {
         'intl':
         False,
     },
-    'hermes_041': {
+    'hermes_090': {
         'download_url':
-        'https://registry.npmjs.org/hermes-engine/-/hermes-engine-0.4.1.tgz',
+        'https://registry.npmjs.org/hermes-engine/-/hermes-engine-0.9.0.tgz',
         'version':
-        '0.4.1',
+        '0.9.0',
         'meta': ('JIT-less', 'bytecode precompile'),
         'aar_glob':
         '**/android/hermes-release.aar',
@@ -72,6 +57,6 @@ JS_DISTS = {
         'maven_dist_path':
         'package/android',
         'intl':
-        False,
+        True,
     },
 }
